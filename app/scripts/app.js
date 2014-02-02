87535166586779de849c81860b9fea34dd117841
Mathias Rieder <mathias.rieder@gmail.com>
2014-02-02 20:46:56 +0100

    added designer view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,27 @@
 'use strict';
 
-angular.module('cgDogCollarsApp', ['ui.bootstrap'
-,  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
+angular.module('cgDogCollarsApp', ['ui.bootstrap',
+   // 'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/collection', {
-        templateUrl: 'views/collection.html',
-        controller: 'CollectionCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        .config(function($routeProvider) {
+            $routeProvider
+                    .when('/', {
+                        templateUrl: 'views/main.html',
+                        controller: 'MainCtrl'
+                    })
+                    .when('/collection', {
+                        templateUrl: 'views/collection.html',
+                        controller: 'CollectionCtrl'
+                    })
+                    .when('/designer', {
+                      templateUrl: 'views/designer.html',
+                      controller: 'DesignerCtrl'
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    });
+        });
